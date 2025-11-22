b7606a3776742c6e7e341f4f286d26427dc5421e
Tiago Matias <tiagomatias072@gmail.com>
2025-11-22 01:49:18 +0100

    fix: resolve merge conflict in ActivityLevel enum

--- types.ts
+++ types.ts
@@ -1,22 +1,14 @@
 export enum Gender {
-  MALE = 'Homem',
-  FEMALE = 'Mulher',
+  MALE = '',
+  FEMALE = 'M',
 }
 
 export enum ActivityLevel {
-<<<<<<< HEAD
-  SEDENTARY = 'Sedentary', // Little or no exercise
-  LIGHT = 'Light', // Light exercise 1-3 days/week
-  MODERATE = 'Moderate', // Moderate exercise 3-5 days/week
-  INTENSE = 'Intense', // Hard exercise 6-7 days/week
-  VERY_INTENSE = 'Very Intense', // Very hard exercise & physical job
-=======
   SEDENTARY = 'Sedentário', // Little or no exercise
   LIGHT = 'Normal', // Light exercise 1-3 days/week
   MODERATE = 'Moderado', // Moderate exercise 3-5 days/week
   INTENSE = 'Intenso', // Hard exercise 6-7 days/week
   VERY_INTENSE = 'Muito intenso', // Very hard exercise & physical job
->>>>>>> 2c18753 (Feito com sucesso)
 }
 
 export enum Somatotype {
@@ -26,17 +18,10 @@ export enum Somatotype {
 }
 
 export enum Goal {
-<<<<<<< HEAD
-  LOSE_FAT = 'Lose Fat',
-  GAIN_MUSCLE = 'Gain Muscle',
-  MAINTAIN = 'Maintain',
-  RECOMP = 'Body Recomposition',
-=======
-  LOSE_FAT = 'Perder peso',
+ LOSE_FAT = 'Perder peso',
   GAIN_MUSCLE = 'Ganhar muscular',
   MAINTAIN = 'Corpo estável',
   RECOMP = 'Corpo perfeito',
->>>>>>> 2c18753 (Feito com sucesso)
 }
 
 export interface UserProfile {
